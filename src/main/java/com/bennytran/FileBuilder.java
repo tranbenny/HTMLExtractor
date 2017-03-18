d6d48a858f899406bfd78486a02b89e63d6a2c0b
Benny Tran <tran.benny123@gmail.com>
2017-03-17 23:53:30 -0700

    Added classes for file generation

---
+++ src/main/java/com/bennytran/FileBuilder.java
@@ -0,0 +1,20 @@
+package com.bennytran;
+
+/**
+ * Created by bennytran on 3/16/17.
+ */
+public class FileBuilder {
+
+    public FileBuilder() {
+
+    }
+
+    public boolean validateFile() {
+        return false;
+    }
+
+    public void writeToFile() {}
+
+    public void generateFile() {}
+
+}
