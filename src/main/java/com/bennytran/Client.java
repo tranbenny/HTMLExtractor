3112ebd7992993e3942f2d8a595cb4be1cfc6e36
Benny Tran <tran.benny123@gmail.com>
2017-03-26 22:26:05 -0700

    fixed main run method and tests so main properly checks for url and filename

--- src/main/java/com/bennytran/Client.java
+++ src/main/java/com/bennytran/Client.java
@@ -2,24 +2,21 @@ package com.bennytran;
 
 
 import java.lang.IllegalArgumentException;
+import java.net.MalformedURLException;
 
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         // validate number of input arguments
         if (args.length != 2) {
             throw new IllegalArgumentException("NOT THE CORRECT NUMBER OF INPUT ARGUMENTS");
         } else {
             String url = args[0];
             String fileName = args[1];
-            HTMLDocument htmlDocument = new HTMLDocument(url);
+            HTMLDocument htmlDocument = null;
+            htmlDocument = new HTMLDocument(url);
             htmlDocument.generateFile(fileName);
         }
-
-//        String url = "https://pitchbook.com/about-pitchbook";
-//        HTMLDocument htmlDocument = new HTMLDocument(url);
-//        htmlDocument.generateFile("sample.txt");
-
     }
 }
 
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -5,16 +5,43 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
+import java.net.MalformedURLException;
+
+import static org.junit.Assert.fail;
 
 
 public class ClientTest {
 
 
-
     @Test(expected = IllegalArgumentException.class)
-    public void testDoesNotRunWithInvalidArgs() {
+    public void testDoesNotRunWithInvalidArgs() throws MalformedURLException {
         String[] args = new String[3];
         Client.main(args);
+    }
+
+    @Test(expected = MalformedURLException.class)
+    public void testRunWithInvalidURL() throws MalformedURLException {
+        String[] args = new String[] {
+            "dsfindfn",
+            "output.txt"
+        };
+        Client.main(args);
+    }
+
+    @Test
+    public void testRunWithValidArguments() {
+        String[] validArgs = new String[] {
+            "https://www.pitchbook.com",
+            "output.txt"
+        };
+        try {
+            Client.main(validArgs);
+            File file = new File(System.getProperty("user.dir") + "/output.txt");
+            file.delete();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            fail("run should not have failed with valid arguments");
+        }
     }
 
 }
